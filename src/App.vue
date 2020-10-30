ec7a3e0cfa9ac792dfb6f17ccf9005047403afdd
maanderz <mandyfung93@gmail.com>
2020-10-30 17:06:09 -0600

    got nav to open

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-	<!-- <Map /> -->
-	<GoogleMap />
+	<Map />
+	<!-- <GoogleMap /> -->
 	<Input />
+	<Nav />
   </div>
 
 
 </template>
 
 <script>
-import GoogleMap from "./components/GoogleMap"
-// import Map from "./components/Map"
+// import GoogleMap from "./components/GoogleMap"
+import Map from "./components/Map"
 import Input from "./components/Input"
+import Nav from "./components/Nav"
 
 export default {
   name: 'App',
   components: {
-	// Map,
+	Map,
 	Input,
-	GoogleMap
+	Nav
+	// GoogleMap
   }
 
 }
