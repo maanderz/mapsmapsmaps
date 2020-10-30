448fb1117c5bcee4970d1e74e08755df4da0c921
maanderz <mandyfung93@gmail.com>
2020-10-30 13:48:35 -0600

    fetched image

---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <input class="address-input"/>
+  </div>
+</template>
+
+<script>  
+  export default {
+    
+  }
+</script>
+
+<style>
+.address-input {
+    background-color: red;
+    display: block;
+    margin: 3rem auto;
+}
+</style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="main-map">
-    <img src="../assets/logo.png"/>
-    <img :src="$store.getters.displayMap" />
+    <img class="map-size" :src="$store.getters.displayMap" />
   </div>
 </template>
 
@@ -14,10 +13,16 @@
     },
     created() {
       this.$store.dispatch('fetchImage');
-
-
-        // .get(`https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=${process.env.VUE_APP_KEY}`)
-        // .then(res => console.log(res.config.url))
-    },
+    }
   }
 </script>
+
+<style>
+  .map-size {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0; 
+    left: 0;
+  }
+</style>
