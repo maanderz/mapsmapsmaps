eb9ce87d14051259770f32caa6d7dbfea1706535
maanderz <mandyfung93@gmail.com>
2020-10-30 14:07:06 -0600

    added default js map

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <Map />
+	<Map />
   </div>
 
 
 </template>
 
 <script>
+// import GoogleMap from "./components/GoogleMap"
 import Map from "./components/Map"
+
 export default {
   name: 'App',
   components: {
-    Map
+	Map
+	// GoogleMap
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store.js'
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_KEY
+  }
+})
 
 new Vue({
   store,
