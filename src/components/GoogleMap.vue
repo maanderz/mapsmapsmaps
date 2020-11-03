9598666c00f23eaa8651e074557cf7180642e354
maanderz <mandyfung93@gmail.com>
2020-11-02 17:32:44 -0700

    first test passed

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -2,63 +2,9 @@
   <div>
     <gmap-map
       :center="$store.getters.displayJSMap.center"
-      :zoom="12"
+      :zoom="15"
       style="width:100vw;  height: 100vh;"
     >
-      <!-- <gmap-marker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center=m.position"
-      ></gmap-marker> -->
     </gmap-map> 
   </div> 
 </template>
-
-<script>
-export default {
-
-//   name: "GoogleMap",
-//   data() {
-    // return {
-    //   // default to Montreal to keep it simple
-    //   // change this to whatever makes sense
-    //   center: { lat: 45.508, lng: -73.587 },
-    //   markers: [],
-    //   places: [],
-    //   currentPlace: null
-//     };
-//   },
-
-//   create() {
-//     this.geolocate();
-//   },
-
-//   methods: {
-//     // receives a place object via the autocomplete component
-//     setPlace(place) {
-//       this.currentPlace = place;
-//     },
-//     addMarker() {
-//       if (this.currentPlace) {
-//         const marker = {
-//           lat: this.currentPlace.geometry.location.lat(),
-//           lng: this.currentPlace.geometry.location.lng()
-//         };
-//         this.markers.push({ position: marker });
-//         this.places.push(this.currentPlace);
-//         this.center = marker;
-//         this.currentPlace = null;
-//       }
-//     },
-//     geolocate: function() {
-//       navigator.geolocation.getCurrentPosition(position => {
-//         this.center = {
-//           lat: position.coords.latitude,
-//           lng: position.coords.longitude
-//         };
-//       });
-//     }
-//   }
-};
-</script>
--- src/store.js
+++ src/store.js
@@ -63,16 +63,6 @@ const mutations = {
 }
 
 const actions = {
-  fetchImage ({ commit }) {
-    axios
-    // .get(`https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=${process.env.VUE_APP_KEY}`)
-    // .then(res => commit('updateMapImage', res.config.url))        
-    
-    .get(`https://api.hatchways.io/assessment/students`)
-    .then(res => (commit('updateMapImage', res.data.students[0].pic)) )
-    .catch(err => console.log(err))  
-  },
-
   fetchAddress({ commit }){
     axios
     .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${state.address}&key=${process.env.VUE_APP_KEY}`)
