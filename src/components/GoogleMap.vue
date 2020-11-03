f8c27b927e24149444fb48f55cda754cd9d3b792
maanderz <mandyfung93@gmail.com>
2020-11-02 17:44:23 -0700

    added in pin

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -5,6 +5,11 @@
       :zoom="15"
       style="width:100vw;  height: 100vh;"
     >
+    <gmap-marker
+      :key="index"
+      :position="$store.getters.displayJSMap.center"
+      >
+    </gmap-marker>
     </gmap-map> 
   </div> 
 </template>
