f9bc57ea2873fb4ba1dffd5dd58a800ffd96edc7
maanderz <mandyfung93@gmail.com>
2020-11-02 18:00:32 -0700

    tested for app mount

---
+++ tests/unit/app.spec.js
@@ -0,0 +1,20 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import App from '../../src/App.vue'
+
+const localVue = createLocalVue();
+
+describe('App', () => {
+    // Checks the App for component object with components
+    it('has components', () => {
+      expect(typeof App.components).toBe('object')
+    })
+})
+
+describe('Mounted App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(App, {
+      localVue
+    });
+  });
+});
--- tests/unit/input.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import App from '../../src/App.vue'
-
-describe('App', () => {
-    // Checks the App for components
-    it('has components', () => {
-      expect(typeof App.components).toBe('object')
-    })
-})
