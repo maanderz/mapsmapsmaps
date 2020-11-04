57187ce9983101fa120efd5549f5de2973d40a36
maanderz <mandyfung93@gmail.com>
2020-11-03 17:13:30 -0700

    initialized database

--- init.sql
+++ init.sql
@@ -1,2 +1,7 @@
+CREATE TABLE addresses (
+  ID SERIAL PRIMARY KEY,
+  address VARCHAR(255) NOT NULL
+);
+
 INSERT INTO addresses (address)
 VALUES  ('188 Kingsway');
--- server.js
+++ server.js
@@ -22,9 +22,6 @@ if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use('/', serveStatic(path.join(__dirname, '/dist')));
 }
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.get('/addresses', (req, res) => {
     console.log('hitting')
