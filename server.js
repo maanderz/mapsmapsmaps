6c96fa3094b7e17476739579673e9d0d90b5614a
maanderz <mandyfung93@gmail.com>
2020-11-03 17:06:34 -0700

    added serve static

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors(origin));
 // app.use('/', serveStatic(path.join(__dirname, '/dist')))
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('client/build'));
+    app.use('/', serveStatic(path.join(__dirname, '/dist')));
 }
 app.get('*',(req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
